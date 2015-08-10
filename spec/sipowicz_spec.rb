8ad7b200bdc1a011937e6e92657d3f4b80aa71a1
Jordan Kamin <jordanakamin@gmail.com>
2015-08-10 06:38:52 -0700

    adds rspec, some pending tests

---
+++ spec/sipowicz_spec.rb
@@ -0,0 +1,23 @@
+describe Sipowicz do
+  describe '#valid_credentials?' do
+    xit 'authenticates a user' do
+    end
+    xit 'returns true if an a user object is authentic' do
+    end
+  end
+  describe '#new_passwords_match?' do
+    xit 'returns true if the user typed their password and confirmation correctly' do
+    end
+    xit 'returns false if the user types their password or confirmation incorrectly' do
+    end
+  end
+  describe '#validate_user' do
+    xit ''
+  end
+  describe '#fields_empty?' do
+  end
+  describe '#validation_redirect' do
+  end
+  describe '#validate_new_passwords' do
+  end
+end
