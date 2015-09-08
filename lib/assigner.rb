07cd4ab56b7159a81962a7302e6a9f6a702652c8
yhordi <jordan@devbootcamp.com>
2015-09-08 15:40:17 -0700

    Tests CopDetectiveAssigner#build_params and #translate_keys.

--- lib/assigner.rb
+++ lib/assigner.rb
@@ -1,6 +1,6 @@
 require 'active_support'
 require 'active_record'
-
+require_relative 'cop_detective'
 class CopDetectiveAssigner
 
   @@keychain = []
@@ -9,18 +9,24 @@ class CopDetectiveAssigner
   class << self
 
     def build_params(params)
+      @@keychain
       params.each do |k, v|
         if v.is_a?(Hash)
           build_params(v)
         end
         @@params[k] = v if @@keychain.include?(k) 
       end
-      p "Params built: " 
-      p @@params
+      @@params
+    end
+
+    def assign(params)
+      build_params(params)
       translate_keys
+      configure
     end
 
     def set_keychain(keys)
+      @@keys = keys
       keys.each do |k, v|
         @@keychain << v
       end
@@ -32,13 +38,16 @@ class CopDetectiveAssigner
       @@internal_keys.each do |k, v|
         @@internal_keys[k] = @@params[v]
       end
-      configure
+      @@internal_keys
     end
 
     def configure
-      @@old_password = @@internal_keys[:old_password]
-      @@password = @@internal_keys[:password] || nil
-      @@confirmation = @@internal_keys[:confirmation]
+      old_password = @@internal_keys[:old_password]
+      password = @@internal_keys[:password] || nil
+      confirmation = @@internal_keys[:confirmation]
+      CopDetective.configure({old_password: old_password, 
+                              password: password, 
+                              confirmation: confirmation})
     end
   end
 end
